15f7be59efbd2f738113e62d4de8747e1d890aa0
renardein <renardein@HOME-PC>
2022-03-24 21:34:34 +0700

    Добавьте файлы проекта.

---
+++ ThesisProject/Forms/LoginForm/LoginForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThesisProject
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+        }
+
+        private void exitLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ ThesisProject/Modules/Crypto/Crypto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThesisProject.Modules.Crypto
+{
+    internal class Crypto
+    {
+        string GetMD5(string value)
+        {
+            var provider = new System.Security.Cryptography.MD5CryptoServiceProvider();
+            var data = Encoding.UTF8.GetBytes(value);
+            data = provider.ComputeHash(data);
+            return Encoding.UTF8.GetString(data);
+        }
+    }
+}
